06e69ead718f3d8a375505a351cc8ad50489b6b1
Alex Amorim <obr.alexamorim@gmail.com>
2026-03-24 03:08:59 -0300

    ajuste tela de detalhe

--- src/app/features/components/imovel/app-detalhe-imovel/app-detalhe-imovel.component.ts
+++ src/app/features/components/imovel/app-detalhe-imovel/app-detalhe-imovel.component.ts
@@ -1,15 +1,40 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { ActivatedRoute, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'acmo-app-detalhe-imovel',
-  imports: [ RouterModule ],
+  imports: [ CommonModule, FormsModule, RouterModule ],
   templateUrl: './app-detalhe-imovel.component.html',
   styleUrl: './app-detalhe-imovel.component.css',
 })
 export class AppDetalheImovelComponent {
   imovelId: string | null = null;
   showDeleteModal = false;
+
+  imovel = {
+    temAreaServico: true,
+    temArmarioCozinha: true,
+    temArmarioQuarto: true,
+    temArmarioProjetado: true,
+    temBoxBanheiro: true,
+    temCeramica: true,
+    temCercado: true,
+    temCloset: true,
+    temCobertura: true,
+    temCondominioFechado: true,
+    temConjugada: true,
+    temCopa: true,
+    temCorredor: true,
+    temCozinha: true,
+    temCozinhaAmericana: true,
+    temDespensa: true,
+    temEscritorio: true,
+    temEsquadra: true,
+    temGradeado: true
+  };
+
 
   constructor(private route: ActivatedRoute) {}
 
